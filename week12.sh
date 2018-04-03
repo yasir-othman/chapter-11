67bc2aac89199e801b61e5f2c5234d6fbe2f459c
yasir-othman <35550050+yasir-othman@users.noreply.github.com>
2018-04-03 14:01:16 -0500

    Create week12.sh

---
+++ week12.sh
@@ -0,0 +1,21 @@
+
+#! /bin/sh
+
+yasir othman
+chapter 11 repo
+
+echo " change the privilege of the repo \n"
+
+chmod 400 week12.sh
+
+echo " print out the output on different lines \n"
+echo -e ' hello friends!! \n hope yall had happy EASTER!!'
+
+echo " lets do some simple math \n"
+echo " the result of 9-3"
+read answer
+if [the result =6]; then
+    echo " the result True"
+else
+    echo " the result False"
+fi
